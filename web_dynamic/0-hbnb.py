e7f247ce8d325ee0631775ac31270734fc897ae9
renefibonacci660 <660@holbertonschool.com>
2019-09-27 23:27:17 +0000

    updated variable cache_id to be able to pass in info on render_template

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -40,7 +40,7 @@ def hbnb_filters(the_id=None):
                            amens=amens,
                            places=places,
                            users=users,
-                           cache_id=uuid4)
+                           cache_id=uuid4())
 
 if __name__ == "__main__":
     """

e8e551db451d6cba529f144ead5b465ce78d21d8
renefibonacci660 <660@holbertonschool.com>
2019-09-30 23:54:58 +0000

    Updated app to use POST request to query Places directly

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
   });
 
   $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus) {
-    if (data.textStatus === "OK") {
+    if (data.status === "OK") {
       $('div#api_status').addClass('available');
     } else {
       $('div#api_status').removeClass('available');
